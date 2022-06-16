139e300c083c7d42d53f0ef1728763caf92a5d14
Kevin Kutzner <kevin.kutzner@hft-stuttgart.de>
2022-06-16 12:11:31 +0200

    setup spring boot maven project

---
+++ src/main/java/de/kebidge/hft/dpt/ccbackend/CcBackendApplication.java
@@ -0,0 +1,13 @@
+package de.kebidge.hft.dpt.ccbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kebidge/hft/dpt/ccbackend/CcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kebidge.hft.dpt.ccbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
